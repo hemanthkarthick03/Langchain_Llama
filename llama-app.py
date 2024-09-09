35a8048c66a2d1e9dd73ae377569622d8dfed092
Hemanth Karthick <128579996+hemanthkarthick03@users.noreply.github.com>
2024-09-09 22:31:47 +0530

    Add files via upload

--- llama-app.py
+++ llama-app.py
@@ -10,8 +10,13 @@ import os
 load_dotenv()
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
+<<<<<<< HEAD:Chatbot/llama-app.py
+os.environ["LANGCHAIN_PROJECT"]="App-LLM"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+=======
 os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+>>>>>>> d934943eec087cb615a61adc096ec4d0c9d7bda9:llama-app.py
 
 ## Prompt Template
 
@@ -27,9 +32,13 @@ st.title("Langchain Demo with Llama")
 input_text = st.text_input("Question need to be searched: ")
 
 # Ollama LLM
-llm = Ollama(model="llama3")
+llm = Ollama(model="llama2")
 output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 
 if input_text:
+<<<<<<< HEAD:Chatbot/llama-app.py
+    st.markdown(chain.invoke({"question": input_text}))
+=======
     st.write(chain.invoke({"question": input_text}))
+>>>>>>> d934943eec087cb615a61adc096ec4d0c9d7bda9:llama-app.py
